7e8888173f07180195b269ca19cf7791d5ed9a46
НаумовСА <@101-04>
2022-10-06 16:33:36 +0300

    Добавьте файлы проекта.

---
+++ List/List.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+struct list {
+	int a;
+	struct lust* next;
+};
+typedef struct list* LOS;
+
+LOS* initLOS(int);
+void printlos(LOS);
+
+
+LOS* initLOS(int n) {
+	LOS los = malloc(1,sizeof(LOS));
+	los->a = 1;
+	LOS p1 = los, p2;
+	for (int i = 0; i < n-1; i++) {
+		p2 = malloc(1, sizeof(LOS));
+		p1->next = p2;
+		p2->a = p1->a + 1;
+		p1 = p2;
+	}
+	p1->next = NULL;
+	return los;
+}
+
+int main()
+{
+	LOS los = initLOS(10);
+	printlos(los);
+
+
+}
+void printlos(LOS los) {
+	while (los)
+	{
+		printf("%d ", los->a);
+		los = los->next;
+	}
+}
+
